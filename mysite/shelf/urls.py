d25b189d3be3dd4a222ccee5229100b017c36c5b
ppyra <przemek1922@vp.pl>
2019-03-24 17:20:01 +0100

    ppyra
    book_details

--- mysite/shelf/urls.py
+++ mysite/shelf/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import AuthorDetailView, AuthorListView, BookListView
+from .views import AuthorDetailView, AuthorListView, BookListView, BookDetailView
 
 urlpatterns = [
 path('authors/', AuthorListView.as_view(), name='author-list'),
 path('authors/<int:pk>', AuthorDetailView.as_view(), name='author-detail'),
 path('ksiazki/', BookListView.as_view(), name='book-list'),
+path('ksiazki/<int:pk>', BookDetailView.as_view(), name='book-detail'),
 ]
--- mysite/shelf/views.py
+++ mysite/shelf/views.py
@@ -10,3 +10,6 @@ class AuthorDetailView(DetailView):
 
 class BookListView(ListView):
     model = Book
+
+class BookDetailView(DetailView):
+    model = Book
